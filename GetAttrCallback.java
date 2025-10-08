cc2c162e56ae0b50f54dc83995eab148b12146a6
Pavel-V1 <pavelvoronov009@gmail.com>
2025-10-08 21:42:56 +0300

    delete

--- GetAttrCallback.java
+++
@@ -1,7 +0,0 @@
-import jnr.ffi.*;
-import jnr.ffi.annotations.Delegate;
-
-public interface GetAttrCallback {
-    @Delegate
-    int getattr(String path, Pointer stbuf);
-}
--- LibFuse.java
+++
@@ -1,5 +0,0 @@
-import jnr.ffi.*;
-
-public interface LibFuse {
-    int fuse_main_real(int argc, String argv[], FuseOperations op, int op_size, Pointer user_data);
-}
