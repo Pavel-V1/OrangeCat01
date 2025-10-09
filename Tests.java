42dc775655fa8da3358c8e9ce2bb8b5dd7ffbea8
Pavel-V1 <pavelvoronov009@gmail.com>
2025-10-09 13:21:54 +0300

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -6,32 +6,34 @@ public class Tests {
         System.out.println("t1");
         mlist.add(0);
         mlist.remove(0);
-        System.out.println("t2");
+        System.out.println(mlist.get(0));
+        System.out.println(mlist.printList(0) + " t2");
         mlist.add(00000000000);
         mlist.add(01);
         mlist.add(2147483647);
-        mlist.remove(0000);
-        System.out.println("t3");
-        mlist.remove(2147483647);
-        System.out.println("t4");
-        for (int i = 0; i < 100; i++) {
-            mlist.add(7);
-            mlist.add(5);
-        }
-        System.out.println("t5");
-        mlist.remove(5);
-        System.out.println("1 nice");
         System.out.println(mlist.printList(0));
+        System.out.println(mlist.get(000));
+        mlist.remove(0000);
+        System.out.println(mlist.get(000));
+        System.out.println(mlist.printList(5) + " t3");
+        System.out.println(mlist.get(2147483647));
+        mlist.remove(2147483647);
+        System.out.println(mlist.get(2147483647));
+        System.out.println(mlist.printList(0) + " t4");
+        mlist.remove(5);
+        mlist.remove(1);
+        System.out.println(mlist.get(0));
+        System.out.println(mlist.printList(0) + " 1 nice");
         mlist.printList();
         mlist.printList(0);
         System.out.println("2 nice");
+        mlist.add(45747);
+        System.out.println(mlist.countMaxElements());
+        System.out.println(mlist.getHead() + " - getHead\n---");
+        System.out.println(mlist.get(45747) + " - get");
+        mlist.remove(45747);
         System.out.println(mlist.countMaxElements());
         System.out.println(mlist.getHead());
-        System.out.println("3 nice");
-        for (int i = 0; i < 100; i++) {
-            System.out.println(mlist.remove(5));
-        }
-        mlist.printList();
-        System.out.println("4 nice");
+        System.out.println("\nAll OK");
     }
 }
