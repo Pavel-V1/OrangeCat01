f8efd91239ca63071be648dd50eba42f211edf53
Pavel-V1 <pavelvoronov009@gmail.com>
2025-10-09 13:20:58 +0300

    Update MyFileSystemNamedMainOrangeLoL.java

--- MyFileSystemNamedMainOrangeLoL.java
+++ MyFileSystemNamedMainOrangeLoL.java
@@ -1,3 +1,31 @@
-public class MyFileSystemNamedMainOrangeLoL {
+import java.util.HashMap;
 
+public class MyFileSystemNamedMainOrangeLoL {
+    private MLinkedList mainList = new MLinkedList();
+    HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+    public void create(Pointer pointer) {
+        mainList.add(pointer); // в значениях value будут храниться адреса
+    }
+
+    public boolean open() {
+
+        return false;
+    }
+
+    public StringBuilder read(String path) {
+        StringBuilder str = new StringBuilder();
+        int value = Integer.valueOf(map.get(path).toString());
+        ListNode node = mainList.get(value);
+//        while (mainList.getNext(node) ... ) {
+            str.append(node.data.toString()); // надо сделать разделение чтобы читать только нужный файл
+            node = mainList.getNext(node);
+//        }
+
+        return str;
+    }
+
+    public void delete(Pointer pointer) {
+        mainList.remove(pointer);
+    } // то есть по сути просто теряем ссылку на файл, но он не удалится же сам?
 }
